825b19e1def85b8db262187dc8b620b14edd0038
crown <867755898@qq.com>
2016-07-07 16:42:56 +0800

    首次提交

---
+++ anotherapp/src/main/java/jike/com/anotherapp/MainActivity.java
@@ -0,0 +1,27 @@
+package jike.com.anotherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnStartService).setOnClickListener(this);
+        findViewById(R.id.btnStopService).setOnClickListener(this);
+        findViewById(R.id.btnBindService).setOnClickListener(this);
+        findViewById(R.id.btnUnbindService).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+        }
+
+    }
+}
